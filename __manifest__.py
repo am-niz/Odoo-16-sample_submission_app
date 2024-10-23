213c7374a91e314f1d270f6673a967bc3ee9003c
nizamudheen <nizamudheenmj@gmail.com>
2024-10-23 20:34:37 +0530

    module documentation is completed, you can view the sample submission documentaion on sample submission module info option

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     """,
 
     'author': "NIZAMUDHEEN MJ",
-    'website': "https://github.com/am-niz   ",
+    'website': "https://github.com/am-niz",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
